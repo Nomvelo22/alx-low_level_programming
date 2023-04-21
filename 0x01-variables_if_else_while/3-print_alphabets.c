3cac71c4a0bd8fbd7207700df6fab301b0c99ea8
Nomvelo22 <mvelowmlotshwa@gmail.com>
2023-04-21 11:35:03 -0700

    print alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main-entry point
+ *Description: 'print alphabet uppercase and lowercase'
+ *Return: Always 0
+ */
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+	}
+	while (m <= 90)
+	{
+		putchar(m)
+	}
+	putchar('\n');
+	return (0);
+}
