7ade3b1abb7591d8adf87d86c0bf07f644194d5a
Nomvelo22 <mvelowmlotshwa@gmail.com>
2023-06-12 12:14:11 -0700

    create file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * Description: read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t tf;
+	ssize_t w;
+	ssize_t t;
+
+	tf = open(filename, O_RDONLY);
+	if (tf == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(tf, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(tf);
+	return (w);
+}
+
+
